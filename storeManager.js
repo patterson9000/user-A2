d9453fdd5f8d4efabef9280d8d7c2f8fb46e75a3
patterson9000 <106724462+patterson9000@users.noreply.github.com>
2022-07-19 18:01:12 -0700

    Create storeManager.js

---
+++ storeManager.js
@@ -0,0 +1,19 @@
+const KEY="users";
+function saveUser(userObj){
+    let oldData=readUser();
+    oldData.push(userObj);
+    let value= JSON.stringify(oldData);
+    //send the user to the local storage
+    localStorage.setItem(KEY,value);
+}
+function readUser(){
+    let data=localStorage.getItem(KEY);
+    console.log(data);//<-jaon
+    if(!data){//is not data
+        return[];
+    }else{
+//we have data
+        let list=JSON.parse(data);
+        return list;
+    }
+}
